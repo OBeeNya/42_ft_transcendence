1e1a367362c56c8a0b34e8976fcaa0915f2204ef
benjamin aubignat <benjamin.aubignat@gmail.com>
2023-04-27 14:49:13 +0200

    fcc almost done

---
+++ src/match-history/match-history.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Patch, UseGuards } from '@nestjs/common';
+import { JwtGuard } from 'src/auth/guard';
+import { MatchHistoryService } from './match-history.service';
+
+// @UseGuards(JwtGuard) // ??? // A logged in user should not be able to freely modify history, so other type of guard to consider
+@Controller('match-history')
+export class MatchHistoryController {
+
+	constructor(private historyService: MatchHistoryService) {}
+
+	@Patch()
+	createHistory() {
+		
+	}
+
+}
