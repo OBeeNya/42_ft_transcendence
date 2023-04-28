393f9c00497679f672f87f00a637f14c7860cee8
felixbarriere <felixbarriere@DESKTOP-ETDM7CT.localdomain>
2023-04-28 16:53:57 +0200

    added back and front folders

--- src/back/match-history/match-history.controller.ts
+++ src/back/match-history/match-history.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Patch, UseGuards } from '@nestjs/common';
-import { JwtGuard } from 'src/auth/guard';
+import { JwtGuard } from '../auth/guard';
 import { MatchHistoryService } from './match-history.service';
 
 // @UseGuards(JwtGuard) // ??? // A logged in user should not be able to freely modify history, so other type of guard to consider
