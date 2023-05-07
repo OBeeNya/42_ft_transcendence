30529eb8478db9f476d7a23c62053b2ad49103e9
Felix Barriere <felix.barriere@gmail.com>
2023-05-07 15:47:06 +0200

    added userName taken message

--- front/src/pages/SignupPage.tsx
+++ front/src/pages/SignupPage.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from 'react-router-dom';
 import { ax } from '../axios/axios'
+import { AxiosError } from 'axios'
 import { AuthDto } from "../../../back/auth/dto";
 
 const SignupPage = () => {
@@ -10,20 +11,32 @@ const SignupPage = () => {
 	
 	const navigate = useNavigate();
 	
-	const handleSignup = () => {
-		const dto: AuthDto = {
-			name: nameInput,
-			password: passwordInput,
-		};
-		ax.post('auth/signup', dto)
-			.then(
-				() => {navigate('/')}
-			)
-			.catch(
-			// display error message	
-			);
-	};
+	const handleSignup = async () => {
+		try {
+			const dto: AuthDto = {
+					name: nameInput,
+					password: passwordInput,
+				};
+			const response = await ax.post('auth/signup', dto);
+			console.log("response status =====>", response.status);
+			if (response.status === 200 || response.status === 201) {
+				navigate('/');
+			}
+		} catch (error) {
+			const axiosError = error as AxiosError<{ message: string; statusCode: number }>;
 
+			if (axiosError?.response?.data?.message === "Credentials taken") {
+				const message = document.getElementById("message");
+				
+				if (message) { message.textContent = "This userName is already taken"; }
+				console.log("This userName is already taken");
+			
+			} else {
+				console.error('Failed to sign up');
+			}
+		}
+	};
+	
 	return (
 		<div>
 			<h1>Signup</h1>
@@ -44,6 +57,7 @@ const SignupPage = () => {
 				/>
 			</div>
 			<button onClick={handleSignup}>Submit</button>
+			<div id="message"></div>
 		</div>
 	);
 
