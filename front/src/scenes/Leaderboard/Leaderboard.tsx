f239eb7f543a1bcf1c919391fde8c9e5e07bb618
Ontiimeyes <lucassalinroge@gmail.com>
2023-05-29 12:05:53 +0200

    Leaderboard usernames fixed

--- front/src/scenes/Leaderboard/Leaderboard.tsx
+++ front/src/scenes/Leaderboard/Leaderboard.tsx
@@ -1,82 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import { useEffect, useState } from "react";
+import { UserInfos } from "../../services/interfaces/userInfos.interface"
+import axios from "axios";
 import './Leaderboard.css';
-import { resolveTripleslashReference } from 'typescript';
 
-function	Leaderboard()
+const Leaderboard = () => 
 {
-	const users =
-	[
-		{username: 'User1', matches: 10, wins: 7, ratio: 0.7, rank: 1, status: 'online'},
-		{username: 'User2', matches: 20, wins: 14, ratio: 0.7, rank: 2, status: 'offline'}
-	];
+	const [users, setUsers] = useState([]);
+	const token = localStorage.getItem("token");
 
-	// type User =
-	// {
-	// 	username: string;
-	// 	matches: number;
-	// 	wins: number;
-	// 	ratio: number;
-	// 	rank: number;
-	// 	status: string;
-	// };
+	useEffect(() =>
+	{
+		const fetchUsers = async () =>
+		{
+			try
+			{
+				const response = await axios.get("http://localhost:8080/users",
+				{
+					headers:
+					{
+						Authorization: `Bearer ${token}`,
+					},
+				});
 
-	// const [users, setUsers] = useState<User[]>([]);
+				setUsers(response.data);
+			}
+			catch (error)
+			{
+				console.error('Error fetching users:', error);
+			}
+		};
 
-	// useEffect(() =>
-	// {
-	// 	axios.get("http://localhost:3000/users")
-	// 		.then(response =>
-	// 		{
-	// 			setUsers(response.data);
-	// 		})
-	// 		.catch(error =>
-	// 		{
-	// 			console.error('There was an error fetching users!', error);
-	// 		});
-	// }, []);
+		fetchUsers();
 
-	return (
-		<div className="content-body leaderboard">
-			<h1>Leaderboard</h1>
-			<table>
-				<thead>
-					<tr>
-						<th>Username</th>
-						<th>Matches Played</th>
-						<th>Wins</th>
-						<th>Ratio</th>
-						<th>Rank</th>
-						<th>Status</th>
-					</tr>
-				</thead>
-				<tbody>
-					{users.map((user, index) => (
-						<tr key={index}>
-							<td>{user.username}</td>
-							<td>{user.matches}</td>
-							<td>{user.wins}</td>
-							<td>{(user.ratio * 100)}%</td>
-							<td>{user.rank}</td>
-							<td className={user.status}>
-								{user.status === 'online' ?
-									<>
-										<div className="online-indicator" />
-										<span>Online</span>
-									</> :
-									<>
-										<div className="offline-indicator" />
-										<span>Offline</span>
-									</>
-								}
-							</td>
-							<td><button>Add Friend</button></td>
-						</tr>
-					))}
-				</tbody>
-			</table>
-		</div>
-	);
+	}, [token]);
+
+  return (
+    <div className="content-body leaderboard">
+      <h1>Leaderboard</h1>
+      <table>
+        <thead>
+          <tr>
+            <th>Username</th>
+            <th>Matches Played</th>
+            <th>Wins</th>
+            <th>Ratio</th>
+            <th>Rank</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+		{users.map((user: UserInfos) =>
+		(
+            <tr key={user.id}>
+              <td>{user.name}</td>
+              <td>{user.wins + user.losses}</td> {/* A ameliorer car ici matches played = simple addition victoires + défaites */}
+              <td>{user.wins}</td>
+              <td>{(user.wins / (user.wins + user.losses) * 100).toFixed(2)}%</td>
+              <td>{user.ladder_level}</td>
+              <td className={user.connected ? 'online' : 'offline'}>
+                {user.connected ?
+                  <>
+                    <div className="online-indicator" />
+                    <span>Online</span>
+                  </> :
+                  <>
+                    <div className="offline-indicator" />
+                    <span>Offline</span>
+                  </>
+                }
+              </td>
+              <td><button>Add Friend</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
 }
 
 export default Leaderboard;
