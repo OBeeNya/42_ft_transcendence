e5bb60890f53ca418a32fbeafad4e641eb56cf21
Felix Barriere <felix.barriere@gmail.com>
2023-06-10 12:29:24 +0200

    Pong playable

---
+++ back/dist/user/user.controller.d.ts
@@ -0,0 +1,24 @@
+import { User } from '@prisma/client';
+import { EditUserDto, QrcodeVerifyDto } from './dto';
+import { UserService } from './user.service';
+export declare class UserController {
+    private userService;
+    constructor(userService: UserService);
+    findAll(): import(".prisma/client").Prisma.PrismaPromise<User[]>;
+    findOne(name: string): Promise<User>;
+    getMe(user: User): User;
+    findOneByName(name: string): Promise<User>;
+    findOneById(id: string): Promise<User>;
+    editUser(userId: number, dto: EditUserDto): Promise<any>;
+    deleteUserById(id: string): Promise<{
+        deletedUsers: number;
+        nbUsers: number;
+    }>;
+    deleteMe(user: User): Promise<{
+        deletedUsers: number;
+        nbUsers: number;
+    }>;
+    uploadAvatar(file: any): any;
+    qrcode(name: any): Promise<string>;
+    verifyCode(elements: QrcodeVerifyDto): Promise<any>;
+}
