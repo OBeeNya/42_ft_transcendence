cb396577f1ea123559837da386015d5e68fe6258
Felix Barriere <felix.barriere@gmail.com>
2023-06-14 19:46:17 +0200

    PONG: added namespace to webSocketGateway

--- back/main.ts
+++ back/main.ts
@@ -4,10 +4,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const serverPong = await NestFactory.create(AppModule);
+//   const serverPong = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({whitelist: true}));
   app.enableCors();
   await app.listen(8080);
-  await serverPong.listen(8081);
+//   await serverPong.listen(8081);
 }
 bootstrap();
