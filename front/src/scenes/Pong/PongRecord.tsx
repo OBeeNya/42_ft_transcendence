b93334511b94bf1ff7813ec79d2b9eaa591b26e2
Benjamin Aubignat <benjamin.aubignat@gmail.com>
2023-06-29 14:07:33 +0200

    wip: match history

--- front/src/scenes/Pong/PongRecord.tsx
+++ front/src/scenes/Pong/PongRecord.tsx
@@ -4,8 +4,18 @@ import { useEffect, useState } from "react";
 
 const PongRecord = () => {
 
-	const navigate = useNavigate();
-	const [currentTime, setCurrentTime] = useState(new Date());
+	const	navigate = useNavigate();
+	const	[currentTime, setCurrentTime] = useState(new Date());
+	const	location = useLocation();
+	const	params = new URLSearchParams(location.search);
+	const	wons = params.get('won');
+	let		won: Boolean;
+	const	token = localStorage.getItem('token');
+	let		response: any;
+	let		exp: number;
+	let		w: number
+	let		l: number
+	let		ladder: number;
 
 	useEffect(() => {
 		const timer = setInterval(() => {
@@ -16,21 +26,18 @@ const PongRecord = () => {
 		};
 	}, []);
 
-    const Record = async () => {
-		const location = useLocation();
-		const params = new URLSearchParams(location.search);
-		const wons = params.get('won');
-		let won: Boolean;
+    const RecordGame = async () => {
 		if (wons === 'true')
 			won = true;
 		else
 			won = false;
 		try {
-			const token = localStorage.getItem('token');
-			const response = await ax.get('http://localhost:8080/users/me', {
+			response = await ax.get('http://localhost:8080/users/me', {
 				headers: { Authorization: `Bearer ${token}` }
             });
-            const ladder = response.data.ladder_level;
+            ladder = response.data.ladder_level;
+            w = response.data.wins;
+            l = response.data.losses;
             await ax.post('match-history', {
 					ladder: ladder,
 					won: won,
@@ -40,38 +47,53 @@ const PongRecord = () => {
 						Authorization: `Bearer ${token}`
 					},
 			});
-			let exp;
-			if (won === true) {
-				await ax.patch('users', {
-					wons: response.data.wons + 1,
-				},
-				{ headers: { Authorization: `Bearer ${token}` }});
-				exp = response.data.exp + 100 / ladder;
-			}
-			else {
-				await ax.patch('users', {
-					losses: response.data.losses + 1,
-				},
-				{ headers: { Authorization: `Bearer ${token}` }});
-				exp = response.data.exp;
-			}
-			if (exp >= 100)
-				await ax.patch('users', {
-					ladder: response.data.ladder + 1,
-					exp: 0,
-				},
-				{ headers: { Authorization: `Bearer ${token}` }});
-			else
-				await ax.patch('users', {
-					exp: exp,
-				},
-				{ headers: { Authorization: `Bearer ${token}` }});
         } catch {
-            console.error('could not add match history');
+            console.error('could not RecordGame');
         }
+    };
+
+	const RecordWinLoss = async () => {
+		// try {
+			console.log(response.data.wins);
+			console.log(response.data.losses);
+			// if (won === true) {
+			// 	await ax.patch('users',
+			// 		{ wins: response.data.wins + 1 },
+			// 		{ headers: { Authorization: `Bearer ${token}` }});
+				// exp = response.data.exp + 100 / ladder;
+			// }
+			// else {
+			// 	await ax.patch('users',
+			// 		{ losses: response.data.losses + 1 },
+			// 		{ headers: { Authorization: `Bearer ${token}` }});
+				// exp = response.data.exp;
+			// }
+        // } catch {
+        //     console.error('could not RecordWinLoss');
+        // }
+    };
+
+	const RecordExp = async () => {
+		// try {
+		// 	if (exp >= 100)
+		// 		await ax.patch('users', {
+		// 				ladder: response.data.ladder + 1,
+		// 				exp: 0,
+		// 			},
+		// 			{ headers: { Authorization: `Bearer ${token}` }});
+		// 	else
+		// 		await ax.patch('users',
+		// 			{ exp: exp },
+		// 			{ headers: { Authorization: `Bearer ${token}` }});
+        // } catch {
+        //     console.error('could not RecordExp');
+        // }
         navigate("/pong");
     };
-    Record();
+
+    RecordGame();
+	RecordWinLoss();
+	RecordExp();
 
 	return (
 		<div>
