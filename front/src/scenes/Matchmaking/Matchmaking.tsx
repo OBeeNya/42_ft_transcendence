719da61bd440c47fba26a5ed1a5ca93bd386814b
benjamin aubignat <benjamin.aubignat@gmail.com>
2023-07-21 10:35:41 +0200

    backspace in matchmaking ok?

--- front/src/scenes/Matchmaking/Matchmaking.tsx
+++ front/src/scenes/Matchmaking/Matchmaking.tsx
@@ -44,11 +44,36 @@ const MatchmakingPage = () => {
         };
     });
 
+    const removePlayer = async () => {
+        try {
+            const response = await ax.get("http://localhost:8080/users/me", {
+                headers: { Authorization: `Bearer ${token}` },
+            });
+            await ax.patch('users',
+                { playing: false },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+            await ax.patch(
+                'pong/removePlayer',
+                { name: response.data.name },
+                { headers: { Authorization: `Bearer ${token}` } }
+            );
+        } catch {
+            console.error('could not remove player when quitting matchmaking');
+        }
+    }
+	
+    const BackButtonListener = ({children}: any) => {
+        removePlayer();
+		return <div></div> ;
+	};
+	
 	return (
         <div className="loading-container">
         <h1 className="loading-text">Waiting for empty room...</h1>
         <div className="loading-circle"></div>
-      </div>
+		<BackButtonListener />
+        </div>
 	);
 
 };
